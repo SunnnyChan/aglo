7dbc780e9717a63c6d5ff09c8d5c4275e3f9b352
sunnychan <sunnnychan@gmail.com>
2018-10-19 15:54:29 +0800

    quick sort

---
+++ src/test/sort/QuickSortTest.java
@@ -0,0 +1,15 @@
+package sort;
+
+import sunny.sort.QuickSort;
+
+/*
+ * Created by sunnnychan@gmail.com on 2018/10/18.
+ */
+
+public class QuickSortTest {
+
+  public static void main(String[] args) {
+    int[] arr = {2, 4, 90, 15, 48};
+    new QuickSort().quickSort(arr);
+  }
+}
